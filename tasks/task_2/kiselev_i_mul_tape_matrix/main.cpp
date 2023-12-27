27c464041a09e3e403c0fed3d057c4e53fd5504a
ISK7 <105171814+ISK7@users.noreply.github.com>
2023-12-27 22:18:37 +0300

    Pull_to_Kis

--- tasks/task_2/kiselev_i_mul_tape_matrix/main.cpp
+++ tasks/task_2/kiselev_i_mul_tape_matrix/main.cpp
@@ -86,47 +86,25 @@ TEST(MulMatrix_test, test_3x4_and_4x3_of_progression) {
     }
 }
 
-TEST(MulMatrix_test, test_4x8_and_8x4_of_random) {
+TEST(MulMatrix_test, test_4x6_and_6x4_of_progression) {
     int rankProc = 0;
     int sizeProc = 0;
     int n = 4;
-    int m = 8;
+    int m = 6;
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rankProc);
     MPI_Comm_size(MPI_COMM_WORLD, &sizeProc);
     std::vector<int> vecA(n * m);
     std::vector<int> vecB(n * m);
     for (int i = 0; i < n * m; i++) {
-        vecA[i] = std::rand() % 10;
-        vecB[i] = std::rand() % 10;
+        vecA[i] = i;
+        vecB[i] = i;
     }
 
     std::vector<int> resSeq = SeqMulMatrix(vecA, vecB, n, m);
 
     if (rankProc == 0) {
         std::vector<int> resPar = ParMulMatrix(&vecA, &vecB, n, m);
-        ASSERT_EQ(resPar, resSeq);
-    }
-}
-
-TEST(MulMatrix_test, test_10x10_of_random) {
-    int rankProc = 0;
-    int sizeProc = 0;
-    int n = 10;
-
-    MPI_Comm_rank(MPI_COMM_WORLD, &rankProc);
-    MPI_Comm_size(MPI_COMM_WORLD, &sizeProc);
-    std::vector<int> vecA(n * n);
-    std::vector<int> vecB(n * n);
-    for (int i = 0; i < n * n; i++) {
-        vecA[i] = std::rand() % 10;
-        vecB[i] = std::rand() % 10;
-    }
-
-    std::vector<int> resSeq = SeqMulMatrix(vecA, vecB, n, n);
-
-    if (rankProc == 0) {
-        std::vector<int> resPar = ParMulMatrix(&vecA, &vecB, n, n);
         ASSERT_EQ(resPar, resSeq);
     }
 }
---
+++ tasks/task_3/kiselev_i_strassen_mul/strassen_mul.h
@@ -0,0 +1,22 @@
+// Copyright 2023 Kiselev Igor
+#ifndef TASKS_TASK_3_KISELEV_I_STRASSEN_MUL_STRASSEN_MUL_H_
+#define TASKS_TASK_3_KISELEV_I_STRASSEN_MUL_STRASSEN_MUL_H_
+#include <mpi.h>
+#include <iostream>
+#include <vector>
+#include <boost/core/invoke_swap.hpp>
+
+std::vector<std::vector<double>> subMatrix(const std::vector<std::vector<double>>& original, int subi, int subj, int size);
+std::vector<std::vector<double>> matrix_addition(const std::vector<std::vector<double>>& A,
+    const std::vector<std::vector<double>>& B);
+std::vector<std::vector<double>> matrix_subtraction(const std::vector<std::vector<double>>& A,
+    const std::vector<std::vector<double>>& B);
+std::vector<std::vector<double>> matrix_multiplication(const std::vector<std::vector<double>>& A,
+    const std::vector<std::vector<double>>& B);
+std::vector<std::vector<double>> merge_matrix(const std::vector<std::vector<double>>& C11,
+                                              const std::vector<std::vector<double>>& C12,
+                                              const std::vector<std::vector<double>>& C21,
+                                              const std::vector<std::vector<double>>& C22);
+std::vector<std::vector<double>> strassen_mult(const std::vector<std::vector<double>>& A,
+                                               const std::vector<std::vector<double>>& B);
+#endif  // TASKS_TASK_3_KISELEV_I_STRASSEN_MUL_STRASSEN_MUL_H_
